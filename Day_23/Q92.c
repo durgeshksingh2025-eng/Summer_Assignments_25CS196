c1864682ec0b250c9343df335b30474bcfac95bc
Durgeshsingh <durgeshk.singh2025@glbajajgroup.org>
2026-06-28 21:54:40 +0530

    Added Day 23 Assignments

---
+++ Day_23/Q92.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j, count, max = 0;
+    char maxChar;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        count = 1;
+
+        if(str[i] == ' ')
+            continue;
+
+        for(j = i + 1; str[j] != '\0'; j++)
+        {
+            if(str[i] == str[j])
+            {
+                count++;
+                str[j] = '*';   // Mark counted character
+            }
+        }
+
+        if(count > max)
+        {
+            max = count;
+            maxChar = str[i];
+        }
+    }
+
+    printf("Maximum occurring character = %c\n", maxChar);
+    printf("Frequency = %d\n", max);
+
+    return 0;
+}
